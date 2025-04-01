f5ec19d22d4048389ad4c8fd9854437e704960d3
ansunghae <ahshss903@gmail.com>
2025-04-01 20:08:27 +0900

    feat :: Add Database
    데이터베이스 테이블을 생성하는 코드와 연결함

---
+++ app.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+import models
+from database import engine
+models.Base.metadata.create_all(bind = engine)
+
+app = FastAPI()
+
+@app.get("/register")
+async def root():
+    return {"message": "Hello World"}
