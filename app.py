5ccb17e9621f9e755c7537126a9f21a12c3b972c
ansunghae <ahshss903@gmail.com>
2025-04-02 15:14:01 +0900

    feat :: 유저라우팅 연결
    유저라우팅을 연결하였음
    - 현재 테스트코드로 업로드한 상태

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
 
+from router import user_router
+
+app.include_router(user_router.app)
+
 @app.get("/register")
 async def root():
     return {"message": "Hello World"}
